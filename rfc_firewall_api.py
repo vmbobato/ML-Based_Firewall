29d85a4ab7c2b02a9fbbb941de41f6c0457aebf0
vmbobato <vini.bobato@hotmail.com>
2025-04-05 19:26:49 -0500

    Updated Flask API

--- rfc_firewall_api.py
+++ rfc_firewall_api.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 
+
 model = joblib.load("rfc_model.pkl")
+feature_names = joblib.load("feature_names.pkl")
 
 app = Flask(__name__)
 
@@ -10,22 +12,21 @@ app = Flask(__name__)
 def predict():
     try:
         data = request.get_json(force=True)
-        input_df = pd.DataFrame([data]) 
 
-        prediction = model.predict(input_df)[0]
-        if prediction == 1:
-            prediction = "Anomaly"
-        else:
-            prediction = "Normal"
-        probability = model.predict_proba(input_df)[0][1]
+        feature_values = [data.get(f, 0) for f in feature_names]
+        input_row = pd.DataFrame([feature_values], columns=feature_names)
 
+        prediction = model.predict(input_row)[0]
+        probability = model.predict_proba(input_row)[0][1]
+
+        prediction = "Anomaly" if prediction == 1 else "Normal"
         return jsonify({
             "prediction": prediction,
             "probability": round(probability, 4)
         })
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
