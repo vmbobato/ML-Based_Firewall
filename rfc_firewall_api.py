40ebae2dc859a49ac67943bf5ce87abdfc106770
Vinicius Bobato <110860928+vmbobato@users.noreply.github.com>
2025-04-07 08:43:18 -0500

    Added protocol encoder

--- rfc_firewall_api.py
+++ rfc_firewall_api.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 model = joblib.load("rfc_model.pkl")
-feature_names = joblib.load("feature_names.pkl")
+column_names = pd.read_csv("Column_Names.txt", header=None).values.flatten()
 
 app = Flask(__name__)
 
@@ -13,8 +13,11 @@ def predict():
     try:
         data = request.get_json(force=True)
 
-        feature_values = [data.get(f, 0) for f in feature_names]
-        input_row = pd.DataFrame([feature_values], columns=feature_names)
+        feature_values = [data.get(f, 0) for f in column_names]
+        input_row = pd.DataFrame([feature_values], columns=column_names)
+
+        protocol_map = {"TCP": 6, "UDP": 17, "ICMP": 1}
+        input_row["Protocol"] = input_row["Protocol"].map(lambda x: protocol_map.get(x.upper(), 0))
 
         prediction = model.predict(input_row)[0]
         probability = model.predict_proba(input_row)[0][1]
@@ -26,7 +29,8 @@ def predict():
         })
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"Error": str(e)}), 400
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
